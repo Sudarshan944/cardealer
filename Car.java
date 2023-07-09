ac21b204ccee46e02a5a1095ef77c33b8a207025
Sudarshan Reddy <88623298+Sudarshan944@users.noreply.github.com>
2023-07-09 23:19:10 +0530

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,14 @@
+public class Car {
+	String name;
+	double price;
+	public Car(String name,double d) {
+		this.name=name;
+		this.price=d;
+	}
+	public String getCarName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
+}
